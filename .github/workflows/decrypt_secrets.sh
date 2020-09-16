7a82259703de20155bb5e0b8ef7c9298bfd76370
nileshdraykar <33151395+nileshdraykar@users.noreply.github.com>
2020-09-16 19:41:00 +0530

    Update decrypt_secrets.sh
    Change path

--- .github/workflows/decrypt_secrets.sh
+++ .github/workflows/decrypt_secrets.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -eo pipefail
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/MegaConference_Appstore_Profile.mobileprovision.mobileprovision ./.github/secrets/MegaConference_Appstore_Profile.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Whiz_Distribution_Certificate_And_Key.p12 ./.github/secrets/Whiz_Distribution_Certificate_And_Key.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.mobileprovision ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12.gpg
 
 # mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/MegaConference_Appstore_Profile.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/MegaConference_Appstore_Profile.mobileprovision.mobileprovision
+cp ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/MegaConference_Appstore_Profile.mobileprovision.mobileprovision
 
 
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/Whiz_Distribution_Certificate_And_Key.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
