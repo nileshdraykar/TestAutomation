8320ecf4516aadd7833479bc06940e3ee7d72a93
nileshdraykar <33151395+nileshdraykar@users.noreply.github.com>
2020-09-17 11:50:23 +0530

    Update decrypt_secrets.sh
    removed password

--- .github/workflows/decrypt_secrets.sh
+++ .github/workflows/decrypt_secrets.sh
@@ -8,8 +8,8 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/
 cp ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/MegaConference_Appstore_Profile.mobileprovision.mobileprovision
 
 
-security create-keychain -p "123456789" build.keychain
-security import ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 -t agg -k ~/Library/Keychains/build.keychain -P "123456789" -A
+security create-keychain -p "" build.keychain
+security import ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
