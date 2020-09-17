42c23556e3d74d918e401ea3744ce20af733b04e
nileshdraykar <33151395+nileshdraykar@users.noreply.github.com>
2020-09-17 12:29:16 +0530

    Update decrypt_secrets.sh
    update file

--- .github/workflows/decrypt_secrets.sh
+++ .github/workflows/decrypt_secrets.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -eo pipefail
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.mobileprovision ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12.gpg
 
 # mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/MegaConference_Appstore_Profile.mobileprovision.mobileprovision
+cp ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/MegaConference_Appstore_Profile.mobileprovision
 
 
 security create-keychain -p "" build.keychain
