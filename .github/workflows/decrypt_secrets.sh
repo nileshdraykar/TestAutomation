98ccd2428918e9c6c7eb4327e4c3fce7fc6fce93
nileshdraykar <33151395+nileshdraykar@users.noreply.github.com>
2020-09-21 11:09:27 +0530

    Update decrypt_secrets.sh
    change profile path

--- .github/workflows/decrypt_secrets.sh
+++ .github/workflows/decrypt_secrets.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -eo pipefail
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/settings/secrets/MegaConference_Appstore_Profile.mobileprovision ./.github/settings/secrets/MegaConference_Appstore_Profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/settings/secrets/Whiz_Distribution_Certificate_And_Key.p12 ./.github/settings/secrets/Whiz_Distribution_Certificate_And_Key.p12.gpg
+
+
+
 
 # mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 lib_folder="./.github/library"
@@ -13,7 +19,7 @@ cp ./.github/workflows/MegaConference_Appstore_Profile.mobileprovision "$provisi
 
 
 security create-keychain -p "" build.keychain
-security import ./.github/workflows/Whiz_Distribution_Certificate_And_Key.p12 -t agg -k $lib_folder/Keychains/build.keychain -P "" -A
+security import ./.github/settings/secrets/Whiz_Distribution_Certificate_And_Key.p12 -t agg -k $lib_folder/Keychains/build.keychain -P "" -A
 
 security list-keychains -s $lib_folder/Keychains/build.keychain
 security default-keychain -s $lib_folder/Keychains/build.keychain
